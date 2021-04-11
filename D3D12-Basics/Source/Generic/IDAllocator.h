4b2de492062529014f6b333fbf2b1b858de5616a
Milan Prem Rowe Singh <milanprsingh@gmail.com>
2021-04-11 12:00:30 -0700

    Made ID Allocator templated on an enum type for ID, for type safetly.

--- D3D12-Basics/Source/Generic/IDAllocator.h
+++ D3D12-Basics/Source/Generic/IDAllocator.h
@@ -1,27 +1,34 @@
 #include "SimpleDequeue.h"
+#include <type_traits>
 
-// Not thread safe! For now
+// Not thread safe! MUST use an enum with an explicitly defined type!
+template <typename IDEnumType>
 class IDAllocator
 {
 public:
-    int32 AllocID()
+    IDAllocator(IDEnumType initialID) :
+        m_nextID(initialID) {}
+
+    IDEnumType AllocID()
     {
         if (m_unusedIDs.IsEmpty())
         {
             return m_unusedIDs.PopBack();
         }
 
-        int32 id = m_nextID++;
+        IDEnumType id = m_nextID;
+        // This is kinda  gross, but I want type safety for the returned IDs 
+        m_nextID = (IDEnumType)((__underlying_type(IDEnumType))m_nextID + 1);
         ASSERT(m_nextID > 0);
-        return m_nextID;
+        return id;
     }
 
-    void FreeID(int32 id)
+    void FreeID(IDEnumType id)
     {
         m_unusedIDs.PushBack(id);
     }
 
 private:
-    SimpleDequeue<int32> m_unusedIDs;
-    int32 m_nextID = -1;
+    SimpleDequeue<IDEnumType> m_unusedIDs;
+    IDEnumType m_nextID;
 };
